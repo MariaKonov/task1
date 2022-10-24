22964bc747b6751129fa7ab451275364be87abab
Maria <marijka000@gmail.com>
2022-10-24 10:32:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int prime = 10456;// стоимости билета,
+        int mile = 20;// рублей для одной бонусной мили
+        System.out.println( prime / mile + " милей будут зачислены за покупку билета");
+    }
+}
